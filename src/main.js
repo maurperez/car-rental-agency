be2dfc8015f81ecf6f1374fc0a345f0f50dbe74c
maurperez <mauro.alejandro.perez@outlook.es>
2020-09-11 14:07:09 -0300

    serve bulma files in public

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 3000
 
 app.use('/public', express.static('public'))
 app.use('/public/css', express.static('node_modules/bulma-carousel/dist/css'))
+app.use('/public/css', express.static('node_modules/bulma/css'))
 app.use('/public/js', express.static('node_modules/bulma-carousel/dist/js'))
 app.set('view engine', 'njk')
 

63eac1d849478a10bcb6faaf380fa77b93979cf2
maurperez <mauro.alejandro.perez@outlook.es>
2020-10-05 15:22:15 -0300

    create customer entity

---
+++ src/module/customer/customer.entity.js
@@ -0,0 +1,17 @@
+require('./types/customer.dto')
+module.exports.Customer = class Customer {
+  /**
+   * @param {customerFromHttpRequestDto} customerInfo 
+   */
+  constructor(customerInfo){
+    this.name = customerInfo.name
+    this.lastName = customerInfo.lastName,
+    this.documentType = customerInfo.documentType,
+    this.nroDocument = customerInfo.nroDocument,
+    this.nationality = customerInfo.nationality,
+    this.address = customerInfo.address,
+    this.phoneNumber = customerInfo.phoneNumber,
+    this.email = customerInfo.email,
+    this.brithDate = customerInfo.birthdate
+  }
+}
---
+++ src/module/customer/types/customer.dto.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {{
+ *  name: string,
+ *  lastName: string,
+ *  documentType: string,
+ *  nroDocument: string,
+ *  nationality: string,
+ *  address: string,
+ *  phoneNumber: string,
+ *  email: string,
+ *  birthdate: Date
+ * }} customerFromHttpRequestDto
+ */
